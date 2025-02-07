30d001d870118d7efd92223de1202b892a755a21
juan <juanperalta2015f@gmail.com>
2025-02-07 16:06:01 -0300

    Agregar archivos de proyecto.

---
+++ Kata.Wallet.Api/Controllers/WalletController.cs
@@ -0,0 +1,22 @@
+﻿using Kata.Wallet.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kata.Wallet.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WalletController : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<List<Domain.Wallet>>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create([FromBody] WalletDto wallet)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Kata.Wallet.Api/Program.cs
@@ -0,0 +1,35 @@
+using Kata.Wallet.Database;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    // serialize enums as strings in api responses 
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+}); ;
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
