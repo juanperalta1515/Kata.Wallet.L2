d1eab2846db0b3799a453decb1419a253a41bf11
juan <juanperalta2015f@gmail.com>
2025-02-11 00:30:22 -0300

    creacion de apis, walletcontroller, walletservice

--- Kata.Wallet.Api/Program.cs
+++ Kata.Wallet.Api/Program.cs
@@ -1,35 +1,29 @@
 using Kata.Wallet.Database;
+using Kata.Wallet.Services;
+
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<DataContext>();
-
-builder.Services.AddControllers().AddJsonOptions(x =>
-{
-    // serialize enums as strings in api responses 
-    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-}); ;
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Agregar servicios
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Registrar servicios de Wallet y Transaction
+builder.Services.AddScoped<IWalletService, WalletService>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
---
+++ Kata.Wallet.Services/IWalletService.cs
@@ -0,0 +1,18 @@
+﻿using Kata.Wallet.Dtos;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Wallet.Services
+{
+    public interface IWalletService
+    {
+        WalletDto CreateWallet(WalletDto walletDto);
+        WalletDto? GetWalletById(int id);
+        WalletDto? GetWalletByUserDocument(string userDocument);
+        IEnumerable<WalletDto> GetWallets(string? currency, string? userDocument);
+    }
+}
