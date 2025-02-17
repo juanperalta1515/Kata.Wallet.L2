5ee61af902618e05c9068bc77782438130b89043
juan <juanperalta2015f@gmail.com>
2025-02-17 17:51:55 -0300

    fix Program.cs

--- Kata.Wallet.Api/Program.cs
+++ Kata.Wallet.Api/Program.cs
@@ -2,13 +2,14 @@ using Kata.Wallet.Database;
 using System.Text.Json.Serialization;
 using Kata.Wallet.Services;
 using Microsoft.EntityFrameworkCore;
+using Kata.Wallet.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configura los servicios para la inyección de dependencias
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseInMemoryDatabase("WalletDb")); // Usa la base de datos en memoria
-IServiceCollection walletServiceRegistration = builder.Services.AddScoped<IWalletService, Wallet>();  // Registra WalletService
+IServiceCollection walletServiceRegistration = builder.Services.AddScoped<IWalletService, WalletService>();  // Registra WalletService
 builder.Services.AddScoped<ITransactionService, TransactionService>(); // Registra TransactionService
 
 builder.Services.AddControllers().AddJsonOptions(x =>
